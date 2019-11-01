ba1ca12493fa7d2d335762c8444277da3aeb938d
Numerica98 <00041517@uca.edu.sv>
2019-11-01 11:28:50 -0600

    Updates

---
+++ precache-manifest.2c1d1beb1b0d49d2fd37d9503a3b2329.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565c2655df14075ba7ef01e4a6eea8ee",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "c4d36c90e808513e28ee",
+    "url": "/tictactoe/static/css/main.67d839ee.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "/tictactoe/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "c4d36c90e808513e28ee",
+    "url": "/tictactoe/static/js/main.b6f2739d.chunk.js"
+  },
+  {
+    "revision": "1dd1878a39f419eb6036",
+    "url": "/tictactoe/static/js/runtime-main.868a150c.js"
+  }
+]);
